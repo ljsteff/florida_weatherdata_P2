b765219aea0b81ad60c745d112d187a264e6460d
hup714 <hcarter1@ufl.edu>
2025-11-04 17:26:26 -0500

    Hope - Adding in front end implementation, fixed missing include statements on map.cpp

---
+++ front_end.cpp
@@ -0,0 +1,11 @@
+#include "front_end.h"
+
+void LoadFont(sf::Font& font, const string& fontFile) {
+    if (!font.openFromFile(fontFile)) {
+        cerr << "failure loading fonts" << endl;
+    }
+}
+
+string getStation(string station) {
+    return station;
+}
