19f2166de58743aba6e549412c48f3658a702f78
ljsteff <leah.steffer@ufl.edu>
2025-11-07 21:02:54 -0500

    Steffer - Resolving some issues with map.cpp, changing ints to long longs to avoid values getting too large.

--- map.cpp
+++ map.cpp
@@ -11,22 +11,21 @@ using namespace std;
 // The map is developed based on parsing the data from the online data set.
 
 // Tiny map helper function:
-map<int,string> createTinyMap(int key, string value){
-    map<int, string> insideMap;
+map<long long,string> createTinyMap(long long key, string value){
+    map<long long, string> insideMap;
     //logic to implement all dates and temps into this map
     insideMap[key] = value;
     return insideMap;
 }
 
 //Updating tiny map helper function:
-map<int,string> updateTinyMap(map<int,string> input, int key, string value) {
-    // maybe check if you need an if statement to check the date?
+void updateTinyMap(map<long long,string> &input, long long key, string value) {
+    // pass by reference
     input[key] = value;
-    return input;
 }
 
 // Helper to get the value of the date/time without dashes or colons:
-int getTime(string selectedYear) {
+long long getTime(string selectedYear) {
     string year;
     istringstream in(selectedYear);
 
@@ -45,18 +44,18 @@ int getTime(string selectedYear) {
     getline(in, minute);
 
     string finalTime = year + month + day + hour + minute;
-    return stoi(finalTime);
+    return stoll(finalTime);
 }
 
 // Setup for the big map:
-map<string, map<int, string>> createMap(const string &metFile){
+map<string, map<long long, string>> createMap(const string &metFile){
     // Need a large map that is the overall data structure holding all of the Florida weather data.
     // The key will be a string that designates the station in Florida that the data is being pulled from.
     // The value will be an additional map that stores two strings.
         // The key to that inside map is a string that designates the specific time of the data point.
         // The value to that inside map is a string, with the rest of the data from the line.
 
-    map<string, map<int,string>> map;
+    map<string, map<long long,string>> map;
 
     // Parsing logic
     ifstream file(metFile);
@@ -80,18 +79,18 @@ map<string, map<int, string>> createMap(const string &metFile){
             map[temp[0]] = createTinyMap(getTime(temp[1]), temp[2]);
         }
         else if (map.find(temp[0]) != map.end()) {
-            map[temp[0]] = updateTinyMap(map[temp[0]], getTime(temp[1]), temp[2]);
+            updateTinyMap(map[temp[0]], getTime(temp[1]), temp[2]);
         }
     }
     return map;
 }
 
 // Input will be the result of the map from the previous function and a range of years?
-vector<pair<int,string>> outputFromMap(map<string, map<int,string>> dataMap, int selectedYearStart, int selectedYearEnd, string selectedStation){
-    vector<pair<int,string>> outputFahrenheit;
+vector<pair<long long,string>> outputFromMap(map<string, map<long long,string>> dataMap, long long selectedYearStart, long long selectedYearEnd, string selectedStation){
+    vector<pair<long long,string>> outputFahrenheit;
     // Search the big map for the station selected.
-    map<int,string> stationMap = dataMap[selectedStation];
-    pair<int,string> stationInfo;
+    map<long long,string> stationMap = dataMap[selectedStation];
+    pair<long long,string> stationInfo;
     // To find a range of key values, use upper and lower bounds. Info here: https://www.geeksforgeeks.org/cpp/searching-map-using-stdmap-functions-c/
     auto it1 = stationMap.lower_bound(selectedYearStart);
     auto it2 = stationMap.upper_bound(selectedYearEnd);
@@ -106,9 +105,9 @@ vector<pair<int,string>> outputFromMap(map<string, map<int,string>> dataMap, int
   return outputFahrenheit;
 }
 
-vector<pair<int,string>> averageCalc(vector<pair<int,string>> input){
+vector<pair<long long,string>> averageCalc(vector<pair<long long,string>> input){
   // Takes the average of all the datapoints for each day and leaves it in the pair
-    vector<pair<int,string>> output;
+    vector<pair<long long,string>> output;
     string store = to_string(input[0].first).substr(0,8);
     vector<int> temp;
     for (auto it = input.begin(); it != input.end(); it++) {
@@ -122,7 +121,7 @@ vector<pair<int,string>> averageCalc(vector<pair<int,string>> input){
     			avg += temp[i];
     		}
     	avg /= temp.size();
-    	output.push_back(make_pair(stoi(store), to_string(avg)));
+    	output.push_back(make_pair(stoll(store), to_string(avg)));
 	 	temp.clear();
     	temp.push_back(stoi(it->second));
     	store = day;
@@ -134,21 +133,20 @@ vector<pair<int,string>> averageCalc(vector<pair<int,string>> input){
         average += temp[j];
     	}
       average /= temp.size();
-      output.push_back(make_pair(stoi(store), to_string(average)));
+      output.push_back(make_pair(stoll(store), to_string(average)));
     }
     return output;
 }
 
-vector<pair<int,string>> celsiusCalc(vector<pair<int,string>> input){
+vector<pair<long long,string>> celsiusCalc(vector<pair<long long,string>> input){
   // Translating all temperture values into Celsius from Fahrenheit
-  vector<pair<int,string>> output;
+  vector<pair<long long,string>> output;
   for (auto it = input.begin(); it != input.end(); it++) {
-    int newTemp = (stoi(it->second)-32)*(5/9);
-    pair<int, string> newPair;
-    newPair = make_pair(it->first, newTemp);
+    int newTemp = static_cast<int>((stoi(it->second)-32)*5.0/9.0);
+    pair<long long, string> newPair;
+    newPair = make_pair(it->first, to_string(newTemp));
     output.push_back(newPair);
   }
   return output;
 }
-
 
